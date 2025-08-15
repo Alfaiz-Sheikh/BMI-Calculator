28f4a0da43305ddcc8d28e84bd898c031a957b59
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-08-15 15:01:06 +0530

    Logic for category Done!

--- app.js
+++ app.js
@@ -10,5 +10,31 @@ bmiForm.addEventListener("submit", function(e) {
 
     let bmi = weight / (height * height);
 
-    bmiValue.innerText = `BMI Value: ${bmi.toFixed(2)}`;
+    bmiValue.innerText = bmi.toFixed(2);
+
+    let result = "";
+
+    switch(true){
+        case (bmi < 18.5):
+            result = "UnderWeight";
+            break;
+
+        case (bmi >= 18.5 && bmi <= 24.9):
+            result = "Normal Weight";
+            break;
+
+        case (bmi >= 25 && bmi <= 29.9):
+            result = "OverWeight";
+            break;
+
+        case (bmi >= 30):
+            result = "Obesity";
+            break;    
+            
+        default:
+            console.log("Invalid Entry!");
+            break;
+    }
+
+    category.innerText = result;
 })
